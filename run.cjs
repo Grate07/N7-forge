6d2d20c9b118502018ed1b6c58a5ca85ff7a019f
Grate <grate00712@gmail.com>
2026-08-19 21:14:15 +0530

    Delete run.cjs

--- run.cjs
+++
@@ -1,9 +0,0 @@
-const { startDiscordBot } = require('./src/index.cjs');
-
-const logger = {
-  info(extra, message) { console.info(message, extra || {}); },
-  warn(extra, message) { console.warn(message, extra || {}); },
-  error(extra, message) { console.error(message, extra || {}); }
-};
-
-startDiscordBot(logger);
